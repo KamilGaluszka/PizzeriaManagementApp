de17a3e4fc76ab882c847b4cfbb09cd064ee1f8b
ga.kamil98@gmail.com <ga.kamil98@gmail.com>
2021-10-10 12:33:31 +0200

    Dodano entity framework, wraz z połączeniem sqlserver. Dodano wstępną migracje z tabelą kategorie.

---
+++ PizzeriaManagementApp/Data/PizzeriaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzeriaManagementApp.Models;
+
+namespace PizzeriaManagementApp.Data
+{
+    public class PizzeriaDbContext : DbContext
+    {
+        public PizzeriaDbContext(DbContextOptions<PizzeriaDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}

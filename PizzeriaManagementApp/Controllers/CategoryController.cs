528f72a3f0017c2520fc66347e8b55c6998ddaeb
ga.kamil98@gmail.com <ga.kamil98@gmail.com>
2021-10-10 15:39:10 +0200

    Dodano kontroler i operacje CRUD dla kategorii. Dodano fontawesome.

---
+++ PizzeriaManagementApp/Controllers/CategoryController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzeriaManagementApp.Data;
+using PizzeriaManagementApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PizzeriaManagementApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly PizzeriaDbContext _dbContext;
+
+        public CategoryController(PizzeriaDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _dbContext.Categories;
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Edit(Guid? id)
+        {
+            if(id == Guid.Empty || id is null)
+            {
+                return NotFound();
+            }
+
+            Category category = _dbContext.Categories.Find(id);
+            if(category is null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == Guid.Empty || id is null)
+            {
+                return NotFound();
+            }
+
+            Category category = _dbContext.Categories.Find(id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirm(Guid? id)
+        {
+            Category category = _dbContext.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Categories.Remove(category);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- PizzeriaManagementApp/Models/Category.cs
+++ PizzeriaManagementApp/Models/Category.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PizzeriaManagementApp.Models
 {
     public class Category
     {
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }

3ebc789427917dad6541767f5e467d0576703c25
ga.kamil98@gmail.com <ga.kamil98@gmail.com>
2021-11-21 13:06:04 +0100

    Dodano individual pizzeria card na stronie głównej.

--- PizzeriaManagementApp/Controllers/HomeController.cs
+++ PizzeriaManagementApp/Controllers/HomeController.cs
@@ -1,26 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using PizzeriaManagementApp.Data;
 using PizzeriaManagementApp.Models;
+using PizzeriaManagementApp.ViewModels;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace PizzeriaManagementApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly PizzeriaDbContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, PizzeriaDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Pizzeria> pizzerias = _dbContext.Pizzerias
+                    .Include(x => x.Address)
+                    .Include(x => x.PizzeriaPizzas)
+                    .ThenInclude(x => x.Pizza)
+                    .ThenInclude(x => x.PizzaProducts)
+                    .ThenInclude(x => x.Product)
+                    .ThenInclude(x => x.Category);
+            IEnumerable<string> towns = _dbContext.Pizzerias
+                .Include(x => x.Address)
+                .OrderBy(x => x.Address.Country)
+                .ThenBy(x => x.Address.Town)
+                .Select(x => x.Address.Town)
+                .Distinct()
+                .ToList();
+            HomeVM homeVM = new()
+            {
+                Pizzerias = pizzerias,
+                Towns = towns
+            };
+            return View(homeVM);
+        }
+
+        public IActionResult Menu(Guid id)
+        {
+            Pizzeria pizzeria = _dbContext.Pizzerias.Where(x => x.Id == id).FirstOrDefault();
+            IEnumerable<Pizza> pizzas = _dbContext.PizzeriaPizzas
+                .Include(x => x.Pizza)
+                .ThenInclude(x => x.PizzaProducts)
+                .ThenInclude(x => x.Product)
+                .Where(x => x.PizzeriaId == id)
+                .Select(x => x.Pizza)
+                .OrderBy(x => x.Price);
+            PizzeriaMenuVM pizzeriaMenuVM = new()
+            {
+                Pizzas = pizzas,
+                Pizzeria = pizzeria
+            };
+            return View(pizzeriaMenuVM);
         }
 
         public IActionResult Privacy()
---
+++ PizzeriaManagementApp/ViewModels/PizzeriaMenuVM.cs
@@ -0,0 +1,11 @@
+﻿using PizzeriaManagementApp.Models;
+using System.Collections.Generic;
+
+namespace PizzeriaManagementApp.ViewModels
+{
+    public class PizzeriaMenuVM
+    {
+        public Pizzeria Pizzeria { get; set; }
+        public IEnumerable<Pizza> Pizzas { get; set; }
+    }
+}
